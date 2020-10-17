1d942fc026915d3ce3f86c966488ef3e67837440
garrettweil <garrett0511@gmail.com>
2020-10-17 15:46:16 -0400

    Working bot

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 import discord
+from discord.ext.commands import Bot
 from discord.ext import commands
+import asyncio
 
 client = commands.Bot(command_prefix="!")
 
 @client.event
 async def on_ready():
     print("Ready!")
+
+@client.command()
+async def helloworld(ctx):
+    await ctx.send("This works!")
     
-client.run("RcJg1EuXvsp51ClRSttYLpGqx_Isoxms")
+client.run("NzY3MTA2NTM5NDIwMjU0MjYw.X4tFow.DWlZ8pmQG_E5J6G2YenVLiFBoCc")
