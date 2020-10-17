ca1d0ae14f6778ad83728c9fa7a66aa9f074bbca
garrettweil <garrett0511@gmail.com>
2020-10-17 15:31:59 -0400

    Added first version of the bot.py script

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("helloworld")
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+    print("Ready!")
+    
+client.run("RcJg1EuXvsp51ClRSttYLpGqx_Isoxms")
