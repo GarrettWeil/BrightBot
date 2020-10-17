326af936f877702eb42e1893e433d0077032a7a2
garrettweil <garrett0511@gmail.com>
2020-10-17 16:12:29 -0400

    command added

--- bot.py
+++ bot.py
@@ -13,6 +13,12 @@ async def on_ready():
 async def helloworld(ctx):
     await ctx.send("This works!")
 
+
+@client.command()
+async def test(ctx,*,message):
+    await ctx.send(message)
+
+
 f = open("token.txt", mode='r')
 f = f.readlines()[0]
 #add your bot's token to the file called token.txt before running 
