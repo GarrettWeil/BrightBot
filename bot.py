d60f436efb37cf14f9d2daf44228ce877bda2670
maxwellscottmartin <70479726+maxwellscottmartin@users.noreply.github.com>
2020-10-17 20:23:45 -0400

    Added quiz scheduler using a temporary JSON file

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
+import json
 
 global classID
 classID = ""
@@ -31,6 +32,29 @@ async def schedule (ctx):
             await ctx.send('Could not find the class, please check your class ID!')
         else:
             await ctx.send('Formatting schedule...')
+
+@client.command()
+async def quizzes (ctx): #unimplemented sections stay out until the user authentication process is completed. For now, uses a temp JSON file
+    if classID == "":
+        await ctx.send('Initialize the class ID first!')
+    else:  
+        # response = requests.get('https://purdue.brightspace.com/d2l/api/le/1.0/' + classID + '/content/toc')
+        # if response.status_code != '200':
+        #     await ctx.send('Error ' + response.status_code + ': Could not find the class, please check your class ID!')
+        # else:
+        await ctx.send('Formatting remaining quizzes...')
+        #    if response.string == "":
+        #        await ctx.send('No quizzes!')
+        #    else:
+        with open('quizzes.json') as data_file: # add two indents
+            data = json.load(data_file)
+        fullResponse = ""
+        for quiz in data["Objects"]:
+            tLoc = quiz["DueDate"].index('T') # used to identify position of 'T' to split up the date and time in the DueDate returned value
+            dueDate = quiz["DueDate"][:tLoc] + ' ' + quiz["DueDate"][tLoc + 1:tLoc + 9] + ' UTC' # 8 more as time format is XX:XX:XX
+            responseString = quiz["Name"] + ': ' + quiz["Description"]["Text"]["Text"] + 'Due: ' + dueDate
+            fullResponse = fullResponse + responseString + '\n\n'
+        await ctx.send(fullResponse)
     
 f = open("token.txt", mode='r')
 f = f.readlines()[0]
